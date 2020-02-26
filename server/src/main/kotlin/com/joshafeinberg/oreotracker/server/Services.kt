c7b6e92583428a5574853b771edfe02b501d2c92
Josh Feinberg <josh@joshafeinberg.com>
2020-02-25 22:17:55 -0600

    Cleanup

--- server/src/main/kotlin/com/joshafeinberg/oreotracker/server/Services.kt
+++ server/src/main/kotlin/com/joshafeinberg/oreotracker/server/Services.kt
@@ -12,15 +12,4 @@ class Services(
 ) : OreoTrackerNetwork,
     SicknessService by sicknessService,
     WeightService by weightService,
-    StatsService by statsService {
-
-    /*override suspend fun getThrowUps(): List<ThrowUp> = sicknessService.getThrowUps()
-
-    override suspend fun postThrowUp(body: ThrowUp) = sicknessService.postThrowUp(body)
-
-    override suspend fun getWeights(): List<Weight> = weightService.getWeights()
-
-    override suspend fun postWeight(body: Weight) = weightService.postWeight(body)
-
-    override suspend fun getStats(): Stats = statsService.getStats()*/
-}
+    StatsService by statsService
